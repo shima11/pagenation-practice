03e8bbbdac01336205d5359213b6880e62174e53
Shima <shima.jin@icloud.com>
2019-09-04 17:02:12 +0900

    Initial Commit

---
+++ pagination-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pagination-practice
+//
+//  Created by jinsei_shima on 2019/09/04.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
